7c341fdc5b4c24053a5506b6bfe7a271d858d2c7
Lucas S. Silva <57202598+Coffee-for-Cats@users.noreply.github.com>
2022-03-10 23:47:38 -0300

    alimentar o pato <3

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-setInterval(move, 100);
-
 const pato = document.querySelector("#Patoso");
-
 console.log(pato);
-
 let margin = 0;
-
 let subtractor = -10;
-
 function move() {
     margin += subtractor;
     if (margin < -40 || margin > 40) {
         subtractor *= -1;
     }
 
-    pato.style["margin-top"] = margin + "px";
+    pato.style["margin-left"] = (margin + 30) + "px";
+}
+
+const alimentado = document.querySelector("#alimentado");
+const alimentarButton = document.querySelector("#alimentar");
+
+const hoje = new Date();
+const diaDeHoje = hoje.getDate()
+
+function alimentar() {
+    setInterval(move, 80);
+    alimentado.textContent = "Seu pato está dançando de felicidade por ter sido alimentado hoje!";
+    alimentarButton.textContent = "Já alimentou ele com amor e carinho!";
+    localStorage.setItem("dia", diaDeHoje);
+}
+
+const ultimaVezAlimentado = localStorage.getItem("dia");
+
+if (ultimaVezAlimentado == diaDeHoje) {
+    alimentar();
 }
